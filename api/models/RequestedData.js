ee876140351979936b886352d1c9d35bd8f65f0e
Braulio Chavez <braulio.vladimir.cn@gmail.com>
2014-05-22 17:42:27 -0600

    Adds model for RequestedData entity

---
+++ api/models/RequestedData.js
@@ -0,0 +1,28 @@
+/**
+ * RequestedData.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+	attributes: {
+    title: {
+      type: 'STRING',
+      required: true
+    },
+    url: {
+      type: 'URL',
+    },
+    description: {
+      type: 'STRING',
+      required: true
+    },
+    votes:{
+      type: 'INTEGER',
+      defaultsTo: 0
+    }
+	}
+
+};
