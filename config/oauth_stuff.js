697f264ed8d37b2d829166bcada33a59d4c7824e
Everardo Padilla <everardo.padilla.saca@gmail.com>
2014-07-25 20:05:50 -0500

    Set the callback URL to point to heroku

--- config/oauth_stuff.js
+++ config/oauth_stuff.js
@@ -3,6 +3,6 @@ module.exports.oauth_stuff = {
   twitter: new twitterAPI({
     consumerKey: 'LEbvogvyw0dhHH3eudgtzUWqt',
     consumerSecret: 'M99MyJkmqqsTlMwiQIKTBNef0Lm4KqqF5oOAFbCeWh9ZpYEXsB',
-    callback: 'http://192.168.1.207:1337/user/authCallback'
+    callback: 'http://apertura-de-datos.herokuapp.com/user/authCallback'
   })
 }
