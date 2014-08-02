65f11616adfc25265d446ea830365b6780c53d3f
Everardo Padilla <everardo.padilla.saca@gmail.com>
2014-08-02 15:29:09 -0500

    Add show newest|most-popular functionality

--- api/controllers/RequestController.js
+++ api/controllers/RequestController.js
@@ -8,6 +8,18 @@
 module.exports = {
 
   index: function(req, res) {
+    function compareVotes(a, b) {
+      if (a.voted.length < b.voted.length)
+        return 1;
+      if (a.voted.length > b.voted.length)
+        return -1;
+      return 0;
+    }
+
+    var newest_li_active = "active";
+    var most_popular_li_active = "";
+    var search_li_active = "";
+
     Request.find({sort: 'createdAt DESC'})
     .populate('voted')
     .exec(function(err, requests) {
@@ -19,27 +31,43 @@ module.exports = {
         User.findOne({id: req.session.user.id})
         .populate('votes')
         .exec(function(err, user) {
-          var userVotes = [];
+          var user_votes = [];
           if (user) {
             for (i in user.votes) {
               // Only grab actual request IDs and not Sails.js
               // add() and remove() functions.
               if (user.votes[i].id) {
-                userVotes.push(user.votes[i].id);
+                user_votes.push(user.votes[i].id);
               }
             }
           }
+          if (req.param('sort_by') == 'most_popular') {
+            requests.sort(compareVotes);
+            newest_li_active = "";
+            most_popular_li_active = "active";
+          }
           return res.view({
             requests: requests,
-            userVotes: userVotes
+            user_votes: user_votes,
+            newest_li_active: newest_li_active,
+            most_popular_li_active: most_popular_li_active,
+            search_li_active: search_li_active
           });
         });
       } else {
         // Current user is not logged-in.
         // Return an empty array for voted requests.
+        if (req.param('sort_by') == 'most_popular') {
+          requests.sort(compareVotes);
+          newest_li_active = "";
+          most_popular_li_active = "active";
+        }
         return res.view({
           requests: requests,
-          userVotes: []
+          user_votes: [],
+          newest_li_active: newest_li_active,
+          most_popular_li_active: most_popular_li_active,
+          search_li_active: search_li_active
         });
       }
     });
@@ -67,4 +95,12 @@ module.exports = {
       return res.redirect('/');
     }
   },
+
+  search: function(req, res) {
+    if (req.method == 'POST' || req.method == 'post') {
+      // query db
+    } else if (req.method == 'GET' || req.method == 'get') {
+      res.view();
+    }
+  }
 };
