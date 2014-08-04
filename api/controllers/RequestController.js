5f8e853c5a91422540f62a5b6ad03e91879e5734
Everardo Padilla <everardo.padilla.saca@gmail.com>
2014-08-04 16:03:41 -0500

    Add basic search

--- api/controllers/RequestController.js
+++ api/controllers/RequestController.js
@@ -5,61 +5,59 @@
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
 
+/*
+ * Computes pagination data.
+ * @param req_param_page {user given, must be int} Contains the requested page number.
+ * @param requests {array of Request objects} All requests in the databse.
+ */
+function getPaginationData(req_param_page, requests) {
+    var pd = {};
+
+    pd.show_prev_page_btn = true;
+    pd.show_next_page_btn = true;
+
+    // Get the amount of possible pages.
+    var pages_float = requests.length / sails.config.globals.cmx.requests_per_page;
+    var pages_int = (pages_float === parseInt(pages_float)) ? pages_float : Math.floor(pages_float) + 1;
+
+    // Always default to page 1.
+    pd.requested_page = 1;
+    if (typeof req_param_page  === 'undefined') { // No page parameter given.
+      // Keep default.
+    } else {
+      if (isNaN(req_param_page)) { // Given page parameter is not a number.
+        // Keep default.
+      } else {  // Given page parameter is a number. Cast to int.
+        pd.requested_page = parseInt(req_param_page);
+      }
+    }
+
+    // Max to the left.
+    if (pd.requested_page <= 1) {
+      pd.requested_page = 1;
+      pd.show_prev_page_btn = false;
+    }
+
+    // Max to the right.
+    if (pd.requested_page >= pages_int) {
+      pd.requested_page = pages_int;
+      pd.show_next_page_btn = false;
+    }
+
+    // Tells the views which pages are back/forward.
+    pd.previous_page = pd.requested_page - 1;
+    pd.next_page = pd.requested_page + 1;
+
+    // Get which requests should be shown in this page.
+    var start = (pd.requested_page - 1) * sails.config.globals.cmx.requests_per_page;
+    pd.paged_requests = requests.slice(start, start + sails.config.globals.cmx.requests_per_page);
+
+    return pd;
+}
+
 module.exports = {
 
   index: function(req, res) {
-
-    /*
-     * Computes pagination data.
-     * @param req_param_page {user given, must be int} Contains the requested page number.
-     * @param requests {array of Request objects} All requests in the databse.
-     */
-    function getPaginationData(req_param_page, requests) {
-        var pd = {};
-
-        pd.show_prev_page_btn = true;
-        pd.show_next_page_btn = true;
-
-        // Get the amount of possible pages.
-        var pages_float = requests.length / sails.config.globals.cmx.requests_per_page;
-        var pages_int = (pages_float === parseInt(pages_float)) ? pages_float : Math.floor(pages_float) + 1;
-
-        // Always default to page 1.
-        pd.requested_page = 1;
-        if (typeof req_param_page  === 'undefined') { // No page parameter given.
-          // Keep default.
-        } else {
-          if (isNaN(req_param_page)) { // Given page parameter is not a number.
-            // Keep default.
-          } else {  // Given page parameter is a number. Cast to int.
-            pd.requested_page = parseInt(req_param_page);
-          }
-        }
-
-        // Max to the left.
-        if (pd.requested_page <= 1) {
-          pd.requested_page = 1;
-          pd.show_prev_page_btn = false;
-        }
-
-        // Max to the right.
-        if (pd.requested_page >= pages_int) {
-          pd.requested_page = pages_int;
-          pd.show_next_page_btn = false;
-        }
-
-        // Tells the views which pages are back/forward.
-        pd.previous_page = pd.requested_page - 1;
-        pd.next_page = pd.requested_page + 1;
-
-        // Get which requests should be shown in this page.
-        var start = (pd.requested_page - 1) * sails.config.globals.cmx.requests_per_page;
-        pd.paged_requests = requests.slice(start, start + sails.config.globals.cmx.requests_per_page);
-
-        return pd;
-    }
-
-
     // Default sorting method is set to 'newest first'.
     var sorting_method = 'newest';
     var newest_li_class = "active";
@@ -150,7 +148,27 @@ module.exports = {
     if (req.method == 'POST' || req.method == 'post') {
       // query db
     } else if (req.method == 'GET' || req.method == 'get') {
-      res.view();
+      Request.find()
+      .where({
+        or: [{
+          url: {contains: req.param('q')}
+        }, {
+          description: {contains: req.param('q')}
+        }, {
+          title: {contains: req.param('q')}
+        }]
+      })
+      .populate('voted')
+      .exec(function(err, requests) {
+        console.log('---->',requests);
+        return res.view('request/index', {
+            user_votes: [],
+            newest_li_class: '',
+            most_votes_li_class: '',
+            sorting_method: 'newest',
+            pd: getPaginationData(0, requests)
+        });
+      });
     }
   }
 };
