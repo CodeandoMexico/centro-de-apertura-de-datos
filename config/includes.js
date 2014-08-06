81cb341e389240b7c5551bb3a655e5885a8d1606
Everardo Padilla <everardo.padilla.saca@gmail.com>
2014-08-05 19:33:54 -0500

    Add moment.js for more human-readable dates. Re-organize includes (twitter oauth and moment).

--- config/includes.js
+++ config/includes.js
@@ -1,8 +1,21 @@
+/*
+  Handles OAuth authentication with Twitter's API.
+*/
 var twitterAPI = require('node-twitter-api');
-module.exports.oauth_stuff = {
+module.exports.oauth = {
   twitter: new twitterAPI({
     consumerKey: process.env.OAUTH_TWITTER_KEY,
     consumerSecret: process.env.OAUTH_TWITTER_SECRET,
     callback: process.env.OAUTH_TWITTER_CALLBACK_HOSTNAME + '/user/authCallback'
   })
+}
+
+
+/*
+  Handles date formatting.
+*/
+var moment = require('moment');
+moment.locale('es-MX');
+module.exports.moment = {
+  moment: moment
 }
