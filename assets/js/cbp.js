25ece316b314f97c7e8f8455b708092f6af49154
Everardo Padilla <everardo.padilla.saca@gmail.com>
2014-08-18 21:05:25 -0500

    Add side menu and files

---
+++ assets/js/cbp.js
@@ -0,0 +1,31 @@
+var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
+        showLeftPush = document.getElementById( 'showLeftPush' ),
+        body = document.body;
+ 
+showLeftPush.onclick = function() {
+    classie.toggle( this, 'active' );
+    classie.toggle( body, 'cbp-spmenu-push-toright' );
+    classie.toggle( menuLeft, 'cbp-spmenu-open' );
+    disableOther( 'showLeftPush' );
+};
+ 
+function disableOther( button ) {
+    if( button !== 'showLeft' ) {
+        classie.toggle( showLeft, 'disabled' );
+    }
+    if( button !== 'showRight' ) {
+        classie.toggle( showRight, 'disabled' );
+    }
+    if( button !== 'showTop' ) {
+        classie.toggle( showTop, 'disabled' );
+    }
+    if( button !== 'showBottom' ) {
+        classie.toggle( showBottom, 'disabled' );
+    }
+    if( button !== 'showLeftPush' ) {
+        classie.toggle( showLeftPush, 'disabled' );
+    }
+    if( button !== 'showRightPush' ) {
+        classie.toggle( showRightPush, 'disabled' );
+    }
+}
