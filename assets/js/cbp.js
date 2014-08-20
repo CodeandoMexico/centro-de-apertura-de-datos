b8892e7154e8f734ab5cc46a0eb871b27a249249
Everardo Padilla <everardo.padilla.saca@gmail.com>
2014-08-19 19:37:00 -0500

    Add glyphicon as button, hide/show appropriate header

--- assets/js/cbp.js
+++ assets/js/cbp.js
@@ -1,31 +1,16 @@
-var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
-        showLeftPush = document.getElementById( 'showLeftPush' ),
-        body = document.body;
+var menuLeft     = document.getElementById('cbp-spmenu-s1');
+var showLeftPush = document.getElementById('show-left-push');
+var body         = document.body;
  
 showLeftPush.onclick = function() {
-    classie.toggle( this, 'active' );
-    classie.toggle( body, 'cbp-spmenu-push-toright' );
-    classie.toggle( menuLeft, 'cbp-spmenu-open' );
-    disableOther( 'showLeftPush' );
+  classie.toggle(this, 'active');
+  classie.toggle(body, 'cbp-spmenu-push-toright');
+  classie.toggle(menuLeft, 'cbp-spmenu-open');
+  disableOther('show-left-push');
 };
  
-function disableOther( button ) {
-    if( button !== 'showLeft' ) {
-        classie.toggle( showLeft, 'disabled' );
-    }
-    if( button !== 'showRight' ) {
-        classie.toggle( showRight, 'disabled' );
-    }
-    if( button !== 'showTop' ) {
-        classie.toggle( showTop, 'disabled' );
-    }
-    if( button !== 'showBottom' ) {
-        classie.toggle( showBottom, 'disabled' );
-    }
-    if( button !== 'showLeftPush' ) {
-        classie.toggle( showLeftPush, 'disabled' );
-    }
-    if( button !== 'showRightPush' ) {
-        classie.toggle( showRightPush, 'disabled' );
-    }
+function disableOther(button) {
+  if(button !== 'show-left-push') {
+    classie.toggle(showLeftPush, 'disabled');
+  }
 }
