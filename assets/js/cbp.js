967ccf41ec235e41ba93eeefbe1304a9e2f1adb8
Everardo Padilla <everardo.padilla.saca@gmail.com>
2014-08-23 15:21:39 -0500

    Make responsive menu hide when clicking outside of it

--- assets/js/cbp.js
+++ assets/js/cbp.js
@@ -1,16 +1,34 @@
-var menuLeft     = document.getElementById('cbp-spmenu-s1');
-var showLeftPush = document.getElementById('show-left-push');
-var body         = document.body;
+var menuLeft        = document.getElementById('cbp-spmenu-s1');
+var showLeftPush    = document.getElementById('show-left-push');
+var body            = document.body;
+var respMenuIsOpen  = false;
  
 showLeftPush.onclick = function() {
   classie.toggle(this, 'active');
   classie.toggle(body, 'cbp-spmenu-push-toright');
   classie.toggle(menuLeft, 'cbp-spmenu-open');
   disableOther('show-left-push');
+  respMenuIsOpen = true;
 };
  
 function disableOther(button) {
   if(button !== 'show-left-push') {
     classie.toggle(showLeftPush, 'disabled');
+  }
+}
+
+document.getElementById('main-content-div').onclick = function() {
+  if (respMenuIsOpen) {
+    classie.toggle(body, 'cbp-spmenu-push-toright');
+    classie.toggle(menuLeft, 'cbp-spmenu-open');
+    respMenuIsOpen = false;
+  }
+}
+
+document.getElementById('intro').onclick = function() {
+  if (respMenuIsOpen) {
+    classie.toggle(body, 'cbp-spmenu-push-toright');
+    classie.toggle(menuLeft, 'cbp-spmenu-open');
+    respMenuIsOpen = false;
   }
 }
