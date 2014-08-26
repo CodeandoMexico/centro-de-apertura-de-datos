4f1d4da1380b177b37e57096866c4fbf7707af85
Everardo Padilla <everardo.padilla.saca@gmail.com>
2014-08-26 04:03:42 -0500

    Add fix

--- assets/js/cbp.js
+++ assets/js/cbp.js
@@ -1,14 +1,12 @@
 var menuLeft        = document.getElementById('cbp-spmenu-s1');
 var showLeftPush    = document.getElementById('show-left-push');
 var body            = document.body;
-var respMenuIsOpen  = false;
  
 showLeftPush.onclick = function() {
   classie.toggle(this, 'active');
   classie.toggle(body, 'cbp-spmenu-push-toright');
   classie.toggle(menuLeft, 'cbp-spmenu-open');
   disableOther('show-left-push');
-  respMenuIsOpen = true;
 };
  
 function disableOther(button) {
@@ -18,17 +16,15 @@ function disableOther(button) {
 }
 
 document.getElementById('main-content-div').onclick = function() {
-  if (respMenuIsOpen) {
+  if (classie.hasClass(menuLeft, 'cbp-spmenu-open')) {
     classie.toggle(body, 'cbp-spmenu-push-toright');
     classie.toggle(menuLeft, 'cbp-spmenu-open');
-    respMenuIsOpen = false;
   }
 }
 
 document.getElementById('intro').onclick = function() {
-  if (respMenuIsOpen) {
+  if (classie.hasClass(menuLeft, 'cbp-spmenu-open')) {
     classie.toggle(body, 'cbp-spmenu-push-toright');
     classie.toggle(menuLeft, 'cbp-spmenu-open');
-    respMenuIsOpen = false;
   }
 }
