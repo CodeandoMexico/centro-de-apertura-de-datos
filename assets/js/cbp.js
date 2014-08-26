20581e5e10e2dcd737b3bed2edbc63a7b13c66f2
Everardo Padilla <everardo.padilla.saca@gmail.com>
2014-08-26 04:45:13 -0500

    Remove extra library and use jquery. Make responsive menu hide when resolution increases

--- assets/js/cbp.js
+++ assets/js/cbp.js
@@ -3,28 +3,48 @@ var showLeftPush    = document.getElementById('show-left-push');
 var body            = document.body;
  
 showLeftPush.onclick = function() {
-  classie.toggle(this, 'active');
-  classie.toggle(body, 'cbp-spmenu-push-toright');
-  classie.toggle(menuLeft, 'cbp-spmenu-open');
+  $(this).toggleClass('active');
+  $(body).toggleClass('cbp-spmenu-push-toright');
+  $(menuLeft).toggleClass('cbp-spmenu-open');
   disableOther('show-left-push');
 };
  
 function disableOther(button) {
   if(button !== 'show-left-push') {
-    classie.toggle(showLeftPush, 'disabled');
+    $(showLeftPush).toggleClass('disabled');
   }
 }
 
 document.getElementById('main-content-div').onclick = function() {
-  if (classie.hasClass(menuLeft, 'cbp-spmenu-open')) {
-    classie.toggle(body, 'cbp-spmenu-push-toright');
-    classie.toggle(menuLeft, 'cbp-spmenu-open');
+  if ($(menuLeft).hasClass('cbp-spmenu-open')) {
+    $(body).toggleClass('cbp-spmenu-push-toright');
+    $(menuLeft).toggleClass('cbp-spmenu-open');
   }
 }
 
 document.getElementById('intro').onclick = function() {
-  if (classie.hasClass(menuLeft, 'cbp-spmenu-open')) {
-    classie.toggle(body, 'cbp-spmenu-push-toright');
-    classie.toggle(menuLeft, 'cbp-spmenu-open');
+  if ($(menuLeft).hasClass('cbp-spmenu-open')) {
+    $(body).toggleClass('cbp-spmenu-push-toright');
+    $(menuLeft).toggleClass('cbp-spmenu-open');
   }
 }
+
+// If the users zooms out (like in a desktop), hide the
+// responsive menu and show the regular one.
+$(document).ready(function() {
+    var $window = $(window);
+
+    function checkWidth() {
+        var windowsize = $window.width();
+        if (windowsize > 1024) {
+            if ($(menuLeft).hasClass('cbp-spmenu-open')) {
+              $(body).toggleClass('cbp-spmenu-push-toright');
+              $(menuLeft).toggleClass('cbp-spmenu-open');
+            }
+        }
+    }
+    // Execute on load
+    checkWidth();
+    // Bind event listener
+    $(window).resize(checkWidth);
+});
