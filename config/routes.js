933c9ad0fc841ddaa445326097e8133629036f5d
Everardo Padilla <everardo.padilla.saca@gmail.com>
2014-08-27 09:37:46 -0500

    Change route /request/view/:id to /solicitud/:id

--- config/routes.js
+++ config/routes.js
@@ -31,6 +31,11 @@ module.exports.routes = {
     action: 'index'
   },
 
+  '/solicitud/:id': {
+    controller: 'request',
+    action: 'view'
+  }
+
   // If a request to a URL doesn't match any of the custom routes above, it is matched 
   // against Sails route blueprints.  See `config/blueprints.js` for configuration options
   // and examples.
